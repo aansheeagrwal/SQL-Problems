7604eb0b928f8136fea501e455641339c5fdfaf5
Anshi <166138746+aansheeagrwal@users.noreply.github.com>
2025-07-14 23:35:04 +0530

    Add files via upload

---
+++ coding ninja sql problem/Shortest Distance in a Plane/db (12).sql
@@ -0,0 +1,4 @@
+CREATE TABLE If Not Exists point_2d (x INT NOT NULL, y INT NOT NULL);
+insert into point_2d (x, y) values ('-1', '-1');
+insert into point_2d (x, y) values ('0', '0');
+insert into point_2d (x, y) values ('-1', '-2');
