df942e625db718066914e6d70bbc1b763480dcf7
Anshi <166138746+aansheeagrwal@users.noreply.github.com>
2025-07-15 09:17:51 +0530

    Add files via upload

---
+++ coding ninja sql problem/Find the Missing IDs/db (14).sql
@@ -0,0 +1,4 @@
+Create table If Not Exists Customers (customer_id int, customer_name varchar(20));
+insert into Customers (customer_id, customer_name) values ('1', 'Alice');
+insert into Customers (customer_id, customer_name) values ('4', 'Bob');
+insert into Customers (customer_id, customer_name) values ('5', 'Charlie');
