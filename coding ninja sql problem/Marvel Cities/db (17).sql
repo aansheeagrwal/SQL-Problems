44b2e9d7b103c799f49f4ddcf7a28c78944b6362
Anshi <166138746+aansheeagrwal@users.noreply.github.com>
2025-07-19 20:53:21 +0530

    Add files via upload

---
+++ coding ninja sql problem/Marvel Cities/db (17).sql
@@ -0,0 +1,13 @@
+Create table If Not Exists CITY (Id int, Name varchar(25), CountryCode varchar(10), Population numeric);
+
+insert into CITY (Id, Name, CountryCode, Population) values ('3878', 'Wakanda', 'Marv', '202705');
+insert into CITY (Id, Name, CountryCode, Population) values ('3981', 'Titan', 'Marv', '212705');
+insert into CITY (Id, Name, CountryCode, Population) values ('3667', 'Starlord', 'Marv', '122705');
+insert into CITY (Id, Name, CountryCode, Population) values ('3771', 'Asgard', 'Marv', '0');
+insert into CITY (Id, Name, CountryCode, Population) values ('375', 'Agamato', 'Marv', '1000000');
+
+
+
+
+
+
