0ac6080f0bbf1a16192d1a6213bd333724d0c310
Anshi <166138746+aansheeagrwal@users.noreply.github.com>
2025-07-19 20:57:33 +0530

    Add files via upload

---
+++ coding ninja sql problem/Duplicate Emails/db (18).sql
@@ -0,0 +1,6 @@
+Create TABLE If Not Exists Person (Id Numeric, Email varchar(255));
+
+
+insert into Person (Id, Email) values ('1', 'a@b.com');
+insert into Person (Id, Email) values ('2', 'c@d.com');
+insert into Person (Id, Email) values ('3', 'a@b.com');
