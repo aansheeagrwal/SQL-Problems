083013683430cb3db1dd2b66bd09dc052a5f0b35
ANSHI GOYAL <166138746+aansheeagrwal@users.noreply.github.com>
2025-08-03 14:40:55 +0530

    Add files via upload

---
+++ coding ninja sql problem/Recyclable and Low Fat Products/db (33).sql
@@ -0,0 +1,10 @@
+CREATE TYPE fats AS ENUM ('Y', 'N');
+CREATE TYPE recycle AS ENUM ('Y','N');
+
+Create table If Not Exists Products (product_id int, low_fats fats, recyclable recycle);
+
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N');
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N');
