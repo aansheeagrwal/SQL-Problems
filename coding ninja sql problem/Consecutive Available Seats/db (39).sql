8b2706daaff90ba6c8d3a44d332f9ca152303cf5
ANSHI GOYAL <166138746+aansheeagrwal@users.noreply.github.com>
2025-08-14 22:07:41 +0530

    Add files via upload

---
+++ coding ninja sql problem/Consecutive Available Seats/db (39).sql
@@ -0,0 +1,7 @@
+Create table If Not Exists cinema (seat_id SERIAL primary key, free bool);
+
+insert into cinema (seat_id, free) values ('1', '1');
+insert into cinema (seat_id, free) values ('2', '0');
+insert into cinema (seat_id, free) values ('3', '1');
+insert into cinema (seat_id, free) values ('4', '1');
+insert into cinema (seat_id, free) values ('6', '1');
