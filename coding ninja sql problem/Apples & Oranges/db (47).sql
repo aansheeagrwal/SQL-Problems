0f6773a275846b8c4f876b265ea53e29a986a258
ANSHI GOYAL <166138746+aansheeagrwal@users.noreply.github.com>
2025-08-25 22:59:48 +0530

    Add files via upload

---
+++ coding ninja sql problem/Apples & Oranges/db (47).sql
@@ -0,0 +1,12 @@
+CREATE TYPE fruit_baskets AS ENUM ('apples','oranges');
+
+Create table If Not Exists Sales (sale_date date, fruit fruit_baskets, sold_num int);
+
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-01', 'apples', '10');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-01', 'oranges', '8');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-02', 'apples', '15');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-02', 'oranges', '15');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-03', 'apples', '20');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-03', 'oranges', '0');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-04', 'apples', '15');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-04', 'oranges', '16');
